1b1c04ad5886949846f74e4622941a1cf0688d1f
Liane Membis <lmembis@gmail.com>
2017-12-26 15:43:39 -0500

    Added method to delete entries

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -16,8 +16,42 @@ require_relative '../models/address_book'
         book = AddressBook.new
         expect(book.entries.size).to eq(0)
       end
-
-
    end
 
- end
+   describe "#add_entry" do
+        it "adds only one entry to the address book" do
+          book = AddressBook.new
+          book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+
+          expect(book.entries.size).to eq(1)
+        end
+
+        it "adds the correct information to entries" do
+          book = AddressBook.new
+          book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+          new_entry = book.entries[0]
+
+          expect(new_entry.name).to eq('Ada Lovelace')
+          expect(new_entry.phone_number).to eq('010.012.1815')
+          expect(new_entry.email).to eq('augusta.king@lovelace.com')
+        end
+      end
+
+    describe "#remove_entry" do
+      it "removes only one entry from the address book" do
+        book = AddressBook.new
+        book.add_entry('Lisa Moore', '010.012.3444', 'lisa.moore@aol.com')
+
+        name = 'Ada Lovelace'
+        phone_number = '010.012.1815'
+        email = 'augusta.king@lovelace.com'
+        book.add_entry(name, phone_number, email)
+        expect(book.entries.size).to eq(2)
+
+        book.remove_entry(name, phone_number, email)
+
+        expect(book.entries.size).to eq(1)
+        expect(book.entries.first.name).to eq('Lisa Moore')
+      end
+    end
+    end
