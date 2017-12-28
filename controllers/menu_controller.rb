940bb967a84aa5eccbd88e23de13aa8a1f0e7ad8
Liane Membis <lmembis@gmail.com>
2017-12-27 21:16:52 -0500

    Added view entry by number feature

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -1,61 +1,66 @@
 require_relative '../models/address_book'
 
  class MenuController
-   attr_reader :address_book
+   attr_accessor :address_book
 
    def initialize
      @address_book = AddressBook.new
    end
 
    def main_menu
-     # #2
-     puts "Main Menu - #{address_book.entries.count} entries"
+ # #2
+     puts "Main Menu - #{@address_book.entries.count} entries"
      puts "1 - View all entries"
      puts "2 - Create an entry"
      puts "3 - Search for an entry"
      puts "4 - Import entries from a CSV"
-     puts "5 - Exit"
+     puts "5 - View Entry Number n"
+     puts "6 - Exit"
      print "Enter your selection: "
 
-     # #3
+ # #3
      selection = gets.to_i
+     puts "You picked #{selection}"
 
-   # #7
      case selection
-       when 1
-         system "clear"
-         view_all_entries
-         main_menu
-       when 2
-         system "clear"
-         create_entry
-         main_menu
-       when 3
-         system "clear"
-         search_entries
-         main_menu
-       when 4
-         system "clear"
-         read_csv
-         main_menu
-       when 5
-         puts "Good-bye!"
-         # #8
-         exit(0)
-       # #9
-       else
-         system "clear"
-         puts "Sorry, that is not a valid input"
-         main_menu
+     when 1
+       system "clear"
+       view_all_entries
+       main_menu
+     when 2
+       system "clear"
+       create_entry
+       main_menu
+     when 3
+       system "clear"
+       search_entries
+       main_menu
+     when 4
+       system "clear"
+       read_csv
+       main_menu
+     when 5
+       system "clear"
+       view_entry_number
+       main_menu
+     when 6
+       puts "Good-bye!"
+ # #8
+       exit(0)
+ # #9
+     else
+       system "clear"
+       puts "Sorry, that is not a valid input"
+       main_menu
      end
    end
 
-   # #10
+ # #10
    def view_all_entries
-     address_book.entries.each do |entry|
+       @address_book.entries.each do |entry|
        system "clear"
        puts entry.to_s
-     # #15
+# #15
        entry_submenu(entry)
      end
 
@@ -63,55 +68,70 @@ require_relative '../models/address_book'
      puts "End of entries"
    end
 
+   def view_entry_number
+       puts "Enter an entry number: "
+       number = gets.chomp.to_i
+
+       if (number <= address_book.entries.length) && (number != 0) && (number.is_a? Integer)
+         puts "#{address_book.entries[number-1].to_s}"
+         puts "Press enter to return to main menu"
+         gets.chomp
+         system "clear"
+       else
+         puts "#{number} is an invalid entry. Please try again."
+         view_entry_number
+       end
+     end
+
+
    def create_entry
      system "clear"
-   puts "New AddressBloc Entry"
-   # #12
-   print "Name: "
-   name = gets.chomp
-   print "Phone number: "
-   phone = gets.chomp
-   print "Email: "
-   email = gets.chomp
+     puts "New AddressBloc Entry"
+# #12
+     print "Name: "
+     name = gets.chomp
+     print "Phone number: "
+     phone = gets.chomp
+     print "Email: "
+     email = gets.chomp
 
-   # #13
-   address_book.add_entry(name, phone, email)
+# #13
+     @address_book.add_entry(name, phone, email)
 
-   system "clear"
-   puts "New entry created"
+     system "clear"
+     puts "New entry created"
    end
 
    def search_entries
    end
 
    def read_csv
- end
- 
- def entry_submenu(entry)
-     # #16
+   end
+
+   def entry_submenu(entry)
+ # #16
      puts "n - next entry"
      puts "d - delete entry"
      puts "e - edit this entry"
      puts "m - return to main menu"
 
-     # #17
+ # #17
      selection = gets.chomp
 
      case selection
-     # #18
-       when "n"
-     # #19
-       when "d"
-       when "e"
-     # #20
-       when "m"
-         system "clear"
-         main_menu
-       else
-         system "clear"
-         puts "#{selection} is not a valid input"
-         entry_submenu(entry)
+ # #18
+     when "n"
+ # #19
+     when "d"
+     when "e"
+ # #20
+     when "m"
+       system "clear"
+       main_menu
+     else
+       system "clear"
+       puts "#{selection} is not a valid input"
+       entries_submenu(entry)
      end
    end
-
-end
+ end
